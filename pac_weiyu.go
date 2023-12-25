9d06db60ac09518debcda87070dbc66ac7d41426
zhaowy1206 <zhaowy1206@gmail.com>
2023-12-25 23:09:58 +0800

    modified:   pac_weiyu.go

--- pac_weiyu.go
+++ pac_weiyu.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"bufio"
-	"database/sql"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -15,33 +12,45 @@ import (
 	"strings"
 	"sync"
 	"time"
-
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 var mu sync.Mutex
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: ./pac_weiyu <function_name> <arg>")
+		fmt.Println("Usage: pac_weiyu.go <function> [arguments]")
+		fmt.Println("\nFunctions:")
+		fmt.Println("  executeAndTime <script> <times> <pacingTime>")
+		fmt.Println("  retrieveStackAndPackLogFiles")
+		fmt.Println("  getStack <coreFile>")
 		os.Exit(1)
 	}
 
-	funcName := os.Args[1]
-
-	// Map function names to function calls
-	funcs := map[string]func(){
-		"monitorLogs":            monitorLogs,
-		"TestDatabaseConnection": TestDatabaseConnection,
-		// Add more functions here
-	}
-
-	// Call the function if it exists in the map
-	if funcCall, ok := funcs[funcName]; ok {
-		funcCall()
-	} else {
-		fmt.Printf("Unknown function: %s\n", funcName)
-		os.Exit(1)
+	// Call the function based on the first argument
+	switch os.Args[1] {
+	case "executeAndTime":
+		if len(os.Args) < 5 {
+			fmt.Println("Usage: pac_weiyu.go executeAndTime <script> <times> <pacingTime>")
+			os.Exit(1)
+		}
+		times, _ := strconv.Atoi(os.Args[3])      // Convert os.Args[3] to int
+		pacingTime, _ := strconv.Atoi(os.Args[4]) // Convert os.Args[4] to int
+		executeAndTime(os.Args[2], times, pacingTime)
+	case "retrieveStackAndPackLogFiles":
+		retrieveStackAndPackLogFiles()
+	case "getStack":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: pac_weiyu.go getStack <coreFile>")
+			os.Exit(1)
+		}
+		stack, err := getStack(os.Args[2])
+		if err != nil {
+			fmt.Printf("Failed to get stack for %s: %v\n", os.Args[2], err)
+			os.Exit(1)
+		}
+		fmt.Printf("Stack for %s: %s\n", os.Args[2], stack)
+	default:
+		fmt.Println("Unknown function:", os.Args[1])
 	}
 }
 
@@ -148,70 +157,82 @@ func tailFile(filename string) {
 	}
 }
 
-func gatherConnectionInfo(r io.Reader) (string, string, string, string, string, error) {
-	reader := bufio.NewReader(r)
-
-	fmt.Print("Enter IP address: ")
-	ip, err := reader.ReadString('\n')
+func executeAndTime(script string, times int, pacingTime int) {
+	logfile, err := os.OpenFile("pac_weiyu.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return "", "", "", "", "", err
+		fmt.Println("Error opening log file:", err)
+		return
 	}
+	for i := 1; i <= times; i++ {
+		start := time.Now()
+		fmt.Fprintf(logfile, "Starting %s run %d at %s\n", script, i, start)
 
-	fmt.Print("Enter port: ")
-	port, err := reader.ReadString('\n')
-	if err != nil {
-		return "", "", "", "", "", err
+		cmd := exec.Command("bash", script)
+		err = cmd.Run()
+		if err != nil {
+			fmt.Fprintf(logfile, "%s failed with exit status %v\n", script, err)
+			break
+		}
+
+		end := time.Now()
+		duration := end.Sub(start)
+		fmt.Fprintf(logfile, "Execution time for %s run %d: %v seconds\n", script, i, duration.Seconds())
+		fmt.Fprintf(logfile, "Ended %s run %d at %s\n", script, i, end)
+
+		time.Sleep(time.Duration(pacingTime) * time.Second)
 	}
-
-	fmt.Print("Enter service name: ")
-	serviceName, err := reader.ReadString('\n')
-	if err != nil {
-		return "", "", "", "", "", err
-	}
-
-	fmt.Print("Enter username: ")
-	username, err := reader.ReadString('\n')
-	if err != nil {
-		return "", "", "", "", "", err
-	}
-
-	fmt.Print("Enter password: ")
-	passwordBytes, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-	if err != nil {
-		return "", "", "", "", "", err
-	}
-	password := string(passwordBytes)
-	fmt.Println() // Print a newline after the password input
-
-	return strings.TrimSpace(ip), strings.TrimSpace(port), strings.TrimSpace(username), strings.TrimSpace(password), strings.TrimSpace(serviceName), nil
 }
 
-func TestDatabaseConnection() {
-	ip, port, username, password, servicename, err := gatherConnectionInfo(os.Stdin)
+func getStack(coreFile string) ([]byte, error) {
+	cmd := exec.Command("./pmx", "-e", coreFile)
+	return cmd.Output()
+}
+
+func retrieveStackAndPackLogFiles() {
+	logfile, err := os.OpenFile("pac_weiyu.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatalf("Error gathering connection info: %v", err)
+		fmt.Println("Error opening log file:", err)
+		return
+	}
+	coreFiles, _ := filepath.Glob("core.*")
+	for _, coreFile := range coreFiles {
+		pid := strings.Split(coreFile, ".")[1]
+		fmt.Fprintf(logfile, "Retrieving stack and packing log files for core file %s\n", coreFile)
+
+		stack, err := getStack(coreFile)
+		if err != nil {
+			fmt.Printf("Failed to get stack for %s: %v\n", coreFile, err)
+			continue
+		}
+		stackFileName := fmt.Sprintf("stack.%s", pid)
+		if len(stack) > 0 && pid != "" {
+			err := ioutil.WriteFile(stackFileName, stack, 0644)
+			if err != nil {
+				fmt.Printf("Failed to write to %s: %v\n", stackFileName, err)
+			}
+		} else {
+			if len(stack) == 0 {
+				fmt.Println("No data to write.")
+			}
+			if pid == "" {
+				fmt.Println("Invalid PID.")
+			}
+		}
+
+		logFiles, _ := filepath.Glob(fmt.Sprintf("logs/*%s*", pid))
+		cmd := exec.Command("zip", "-r", fmt.Sprintf("stack_and_log_%s.zip", pid), fmt.Sprintf("stack.%s", pid))
+		cmd.Args = append(cmd.Args, logFiles...)
+		cmd.Run()
+
+		os.Remove(fmt.Sprintf("stack.%s", pid))
 	}
 
-	connStr := fmt.Sprintf("%s/%s@%s:%s/%s", username, password, ip, port, servicename)
+	finalZipFile := fmt.Sprintf("final_stack_and_log_%s.zip", time.Now().Format("20060102_150405"))
+	cmd := exec.Command("zip", "-r", finalZipFile, "stack_and_log_*.zip")
+	cmd.Run()
 
-	// Attempt to connect to the database
-	db, err := sql.Open("godror", connStr)
-	if err != nil {
-		fmt.Println("Failed to connect to the database: %v", err)
-	}
-
-	var count float64
-	err = db.QueryRow("SELECT COUNT(*) FROM v$session").Scan(&count)
-	if err != nil {
-		fmt.Printf("Failed to run the SQL: %v\n", err)
-		fmt.Printf("The connStr was: %v\n", connStr)
-	} else {
-		fmt.Printf("The number of sessions is: %f\n", count)
-	}
-
-	// Close the connection
-	err = db.Close()
-	if err != nil {
-		fmt.Println("Failed to close the database connection: %v", err)
+	files, _ := filepath.Glob("stack_and_log_*.zip")
+	for _, file := range files {
+		os.Remove(file)
 	}
 }
