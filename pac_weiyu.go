325e4ec92e6ec40cdbdf3b9fac4cbd93df757350
zhaowy1206 <zhaowy1206@gmail.com>
2023-12-26 11:02:59 +0800

    modified:   pac_weiyu.go 	modified:   pac_weiyu.py

--- pac_weiyu.go
+++ pac_weiyu.go
@@ -15,6 +15,16 @@ import (
 )
 
 var mu sync.Mutex
+var logfile *os.File
+
+func init() {
+	var err error
+	logfile, err = os.OpenFile("pac_weiyu.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("Error opening log file: %v\n", err)
+		os.Exit(1)
+	}
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -23,6 +33,7 @@ func main() {
 		fmt.Println("  executeAndTime <script> <times> <pacingTime>")
 		fmt.Println("  retrieveStackAndPackLogFiles")
 		fmt.Println("  getStack <coreFile>")
+		fmt.Println("  writeStackToFile <coreFile>")
 		os.Exit(1)
 	}
 
@@ -49,8 +60,28 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("Stack for %s: %s\n", os.Args[2], stack)
+	case "writeStackToFile":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: pac_weiyu.go writeStackToFile <coreFile>")
+			os.Exit(1)
+		}
+		err := writeStackToFile(os.Args[2])
+		if err != nil {
+			fmt.Printf("Error writing stack to file: %v\n", err)
+			os.Exit(1)
+		}
+
 	default:
 		fmt.Println("Unknown function:", os.Args[1])
+	}
+
+	defer logfile.Close()
+}
+
+func writeLog(logfile *os.File, message string) {
+	_, err := fmt.Fprint(logfile, message)
+	if err != nil {
+		fmt.Printf("Error writing to log file: %v\n", err)
 	}
 }
 
@@ -158,26 +189,25 @@ func tailFile(filename string) {
 }
 
 func executeAndTime(script string, times int, pacingTime int) {
-	logfile, err := os.OpenFile("pac_weiyu.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println("Error opening log file:", err)
-		return
-	}
 	for i := 1; i <= times; i++ {
 		start := time.Now()
-		fmt.Fprintf(logfile, "Starting %s run %d at %s\n", script, i, start)
+		message := fmt.Sprintf("Starting %s run %d at %s\n", script, i, start)
+		writeLog(logfile, message)
 
 		cmd := exec.Command("bash", script)
-		err = cmd.Run()
+		err := cmd.Run()
 		if err != nil {
-			fmt.Fprintf(logfile, "%s failed with exit status %v\n", script, err)
+			message := fmt.Sprintf("%s failed with exit status %v\n", script, err)
+			writeLog(logfile, message)
 			break
 		}
 
 		end := time.Now()
 		duration := end.Sub(start)
-		fmt.Fprintf(logfile, "Execution time for %s run %d: %v seconds\n", script, i, duration.Seconds())
-		fmt.Fprintf(logfile, "Ended %s run %d at %s\n", script, i, end)
+		message = fmt.Sprintf("Execution time for %s run %d: %v seconds\n", script, i, duration.Seconds())
+		writeLog(logfile, message)
+		message = fmt.Sprintf("Ended %s run %d at %s\n", script, i, end)
+		writeLog(logfile, message)
 
 		time.Sleep(time.Duration(pacingTime) * time.Second)
 	}
@@ -188,50 +218,115 @@ func getStack(coreFile string) ([]byte, error) {
 	return cmd.Output()
 }
 
-func retrieveStackAndPackLogFiles() {
-	logfile, err := os.OpenFile("pac_weiyu.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func getPid(coreFile string) string {
+	pid := strings.Split(coreFile, ".")[1]
+	return pid
+}
+
+func writeStackToFile(coreFile string) error {
+	pid := getPid(coreFile)
+	stack, err := getStack(coreFile)
 	if err != nil {
-		fmt.Println("Error opening log file:", err)
-		return
+		writeLog(logfile, fmt.Sprintf("Failed to get stack for %s: %v\n", coreFile, err))
+		return fmt.Errorf("Failed to get stack for %s: %v", coreFile, err)
 	}
+
+	stackFileName := fmt.Sprintf("stack.%s", pid)
+	if len(stack) > 0 && pid != "" {
+		err := ioutil.WriteFile(stackFileName, stack, 0644)
+		if err != nil {
+			writeLog(logfile, fmt.Sprintf("Failed to write to %s: %v\n", stackFileName, err))
+			return fmt.Errorf("Failed to write to %s: %v", stackFileName, err)
+		}
+		writeLog(logfile, fmt.Sprintf("Successfully wrote to %s\n", stackFileName))
+	} else {
+		if len(stack) == 0 {
+			writeLog(logfile, "No data to write.\n")
+		}
+		if pid == "" {
+			writeLog(logfile, "Invalid PID.\n")
+		}
+	}
+	return nil
+}
+
+func findLogFiles(pid string) ([]string, error) {
+	var logFiles []string
+	err := filepath.Walk("logs", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.Contains(path, pid) {
+			logFiles = append(logFiles, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return logFiles, nil
+}
+
+func retrieveStackAndPackLogFiles() {
 	coreFiles, _ := filepath.Glob("core.*")
 	for _, coreFile := range coreFiles {
-		pid := strings.Split(coreFile, ".")[1]
-		fmt.Fprintf(logfile, "Retrieving stack and packing log files for core file %s\n", coreFile)
+		pid := getPid(coreFile)
+		message := fmt.Sprintf("Retrieving stack and packing log files for core file %s\n", coreFile)
+		writeLog(logfile, message)
 
-		stack, err := getStack(coreFile)
+		err := writeStackToFile(coreFile)
 		if err != nil {
-			fmt.Printf("Failed to get stack for %s: %v\n", coreFile, err)
-			continue
-		}
-		stackFileName := fmt.Sprintf("stack.%s", pid)
-		if len(stack) > 0 && pid != "" {
-			err := ioutil.WriteFile(stackFileName, stack, 0644)
-			if err != nil {
-				fmt.Printf("Failed to write to %s: %v\n", stackFileName, err)
-			}
-		} else {
-			if len(stack) == 0 {
-				fmt.Println("No data to write.")
-			}
-			if pid == "" {
-				fmt.Println("Invalid PID.")
-			}
+			fmt.Println(err)
 		}
 
-		logFiles, _ := filepath.Glob(fmt.Sprintf("logs/*%s*", pid))
-		cmd := exec.Command("zip", "-r", fmt.Sprintf("stack_and_log_%s.zip", pid), fmt.Sprintf("stack.%s", pid))
-		cmd.Args = append(cmd.Args, logFiles...)
-		cmd.Run()
+		logFiles, err := findLogFiles(pid)
+		if err != nil {
+			writeLog(logfile, fmt.Sprintf("Failed to find log files: %v\n", err))
+			return
+		}
+		writeLog(logfile, fmt.Sprintf("Log files: %s\n", strings.Join(logFiles, ", ")))
+
+		args := append([]string{"zip", "-r", fmt.Sprintf("stack_and_log_%s.zip", pid), fmt.Sprintf("stack.%s", pid)}, logFiles...)
+		cmd := exec.Command(args[0], args[1:]...)
+
+		writeLog(logfile, fmt.Sprintf("Running command: %s\n", strings.Join(cmd.Args, " ")))
+		cmdOutput, err := cmd.CombinedOutput()
+		if err != nil {
+			writeLog(logfile, fmt.Sprintf("Failed to zip files: %v\n", err))
+		} else {
+			writeLog(logfile, fmt.Sprintf("Zip command output: %s\n", cmdOutput))
+		}
 
 		os.Remove(fmt.Sprintf("stack.%s", pid))
 	}
 
-	finalZipFile := fmt.Sprintf("final_stack_and_log_%s.zip", time.Now().Format("20060102_150405"))
-	cmd := exec.Command("zip", "-r", finalZipFile, "stack_and_log_*.zip")
-	cmd.Run()
+	files, err := filepath.Glob("stack_and_log_*.zip")
+	if err != nil {
+		writeLog(logfile, fmt.Sprintf("Failed to find files: %v\n", err))
+		return
+	}
 
-	files, _ := filepath.Glob("stack_and_log_*.zip")
+	if len(files) == 0 {
+		writeLog(logfile, "No files match the pattern stack_and_log_*.zip\n")
+		return
+	}
+
+	writeLog(logfile, fmt.Sprintf("%d files match the pattern stack_and_log_*.zip\n", len(files)))
+
+	finalZipFile := fmt.Sprintf("final_stack_and_log_%s.zip", time.Now().Format("20060102_150405"))
+	cmd := exec.Command("zip", "-r", finalZipFile)
+	cmd.Args = append(cmd.Args, files...)
+
+	writeLog(logfile, fmt.Sprintf("Running command: %s\n", strings.Join(cmd.Args, " ")))
+	cmdOutput, err := cmd.CombinedOutput()
+	if err != nil {
+		writeLog(logfile, fmt.Sprintf("Failed to create final zip file: %v\n", err))
+	} else {
+		writeLog(logfile, fmt.Sprintf("Successfully created final zip file: %s\n", finalZipFile))
+		writeLog(logfile, fmt.Sprintf("Zip command output: %s\n", cmdOutput))
+	}
+
+	files, _ = filepath.Glob("stack_and_log_*.zip")
 	for _, file := range files {
 		os.Remove(file)
 	}
